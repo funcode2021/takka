f0f7d2eb4f11a9dbbb9fcb5989a8f1be8949d215
funcode2021 <73496351+funcode2021@users.noreply.github.com>
2026-04-01 23:35:01 +0200

    minor security fix

--- src/components/JsonLd/JsonLd.tsx
+++ src/components/JsonLd/JsonLd.tsx
@@ -1,12 +1,14 @@
 interface JsonLdProps {
-  schema: Record<string, unknown>
+  schema: Record<string, unknown>;
 }
 
 export default function JsonLd({ schema }: JsonLdProps) {
   return (
     <script
       type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(schema).replace(/</g, "\\u003c"),
+      }}
     />
-  )
+  );
 }
